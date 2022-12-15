a9a2a9048c2d02a64599f7d6f67adc48c9f5b741
Matthias Budde <buddemat@users.noreply.github.com>
2022-12-15 15:58:05 +0100

    Chore: Updated helper script to update remote workspace.

--- init_udacity_workspace.sh
+++ init_udacity_workspace.sh
@@ -1,4 +1,10 @@
-sudo apt update
-sudo apt install --only-upgrade firefox
+#!/usr/bin/env bash
+
+# update firefox to prevent crashes
+sudo apt-get update
+sudo apt-get install --only-upgrade firefox
+
+# configure git repo locally
 git config user.name "Matthias Budde"
 git config user.email "buddemat@users.noreply.github.com"
+
