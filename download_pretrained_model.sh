47b5d55433355f07fd7d995cc12af5e46aec1fe8
Matthias Budde <buddemat@users.noreply.github.com>
2022-12-15 16:45:18 +0100

    Feat: Added calls to edit configuration.

--- download_pretrained_model.sh
+++ download_pretrained_model.sh
@@ -5,3 +5,7 @@ cd /home/workspace/experiments/pretrained_model/
 wget http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz
 tar -xvzf ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz
 rm -rf ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz
+
+# edit config files to change location of training, validation and label_map files, also adjust batch size
+cd /home/workspace/
+python edit_config.py --train_dir /home/workspace/data/train/ --eval_dir /home/workspace/data/val/ --batch_size 2 --checkpoint /home/workspace/experiments/pretrained_model/ssd_resnet50_v1_fpn_640x640_coco17_tpu-8/checkpoint/ckpt-0 --label_map /home/workspace/experiments/label_map.pbtxt
